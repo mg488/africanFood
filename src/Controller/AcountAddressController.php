d69f39bb30ec16986c989bfc3b62b6ad2616f34b
mg488 <contactvillagedenguith@gmail.com>
2021-11-18 23:13:09 +0100

    gestion des adresses des utilisateurs

--- src/Controller/AcountAddressController.php
+++ src/Controller/AcountAddressController.php
@@ -4,12 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Address;
 use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AcountAddressController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+
+        $this->entityManager=$entityManager;
+    }
     /**
      * @Route("/compte/adresses", name="acount_address")
      */
@@ -22,10 +33,48 @@ class AcountAddressController extends AbstractController
     /**
      * @Route("/compte/ajouter-une-adresse", name="acount_address_add_new")
      */
-    public function addNewAddressAction(): Response
+    public function addNewAddressAction(Request $request): Response
     {
         $address = new Address();
         $form = $this->createForm(AddressType::class,$address);
+
+        $form=$form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $address = $form->getData();
+            $address->setUser($this->getUser());
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('acount_address');
+        }
+        return $this->render('account/address_add_new.html.twig',[
+        'form'=>$form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/compte/modifier-une-adresse/{id}", name="acount_address_edit")
+     */
+    public function editAddressAction(Request $request, $id): Response
+    {
+
+        $address= $this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        if(!$address || $address->getUser()!=$this->getUser()){
+            return $this->redirectToRoute('acount_address');
+        }
+
+        $form =$this->createForm(AddressType::class,$address);
+        //lamis àjour est faite directement avec le handleRequest
+        $form=$form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $this->entityManager->flush();
+            return $this->redirectToRoute('acount_address');
+        }
         return $this->render('account/address_add_new.html.twig',[
         'form'=>$form->createView()
         ]);
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Address;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +15,66 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('comapny')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
+            ->add('name',TextType::class,[
+                'label'=>'Quel nom souhaitez-vous donner à votre adresse ?',
+                'required'=>true,
+                'attr'=>[
+                    'placeholder'=>'Nommez votre adresse'
+                ]
+            ])
+            ->add('firstname',TextType::class,[
+                'label'=>'Votre Nom',
+                'attr'=>[
+                    'placeholder'=>'ici votre nom'
+                ]
+            ])
+            ->add('lastname',TextType::class,[
+                'label'=>'Votre Prénom',
+                'attr'=>[
+                    'placeholder'=>'ici votre prénom'
+                ]
+            ])
+            ->add('comapny',TextType::class,[
+                'label'=>'Votre Société',
+                'required'=>false,
+                'attr'=>[
+                    'placeholder'=>'(facultatif) ici votre société'
+                ]
+            ])
+
+            ->add('address',TextType::class,[
+                'label'=>'votre adresse',
+                'attr'=>[
+                    'placeholder'=>'exemple : 1 rue Abdoulaye NDIAYE ...'
+                ]
+            ])
+            ->add('postal',TextType::class,[
+                'label'=>'Votre Code Posyal',
+                'attr'=>[
+                    'placeholder'=>'ici votre code postal'
+                ]
+            ])
+            ->add('city',TextType::class,[
+                'label'=>'Ville',
+                'attr'=>[
+                    'placeholder'=>'ici votre ville'
+                ]
+            ])
+            ->add('country',CountryType::class,[
+                'label'=>'Pays'
+            ])
+
+            ->add('phone',TextType::class,[
+                'label'=>'Votre Téléphone',
+                'attr'=>[
+                    'placeholder'=>'ici votre phone'
+                ]
+            ])
             ->add('submit',SubmitType::class,[
-                'label'=>'Ajouter mon adresse'
+                'label'=>'Ajouter mon adresse',
+                'attr'=>[
+                    'class'=>'btn btn-info float-end'
+    ]
             ])
 
         ;
